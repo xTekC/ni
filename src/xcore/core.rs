6aa8e3e4f39a1ae9e9891d4be21a5c98a348efd7
xTekC <xtekc.dev@pm.me>
2024-01-15 00:40:59 -0500

    chore(core):update

--- src/xcore/core.rs
+++ src/xcore/core.rs
@@ -4,3 +4,44 @@
  *        See LICENSE for details.        *
  * https://www.mozilla.org/en-US/MPL/2.0/ *
  ******************************************/
+
+use pnet::datalink;
+use std::net::IpAddr;
+
+pub fn interface_name() {
+    for iface in datalink::interfaces() {
+        let name = iface.name;
+        println!("{}", name);
+    }
+}
+
+pub fn status_of_interface() {
+    for iface in datalink::interfaces() {
+        let status = iface.is_up();
+        println!("Up: {}", status);
+    }
+}
+
+pub fn mac_address() {
+    for iface in datalink::interfaces() {
+        let maca = match iface.mac {
+            Some(mac) => mac.to_string(),
+            None => String::from("None"),
+        };
+        println!("MAC: {}", maca);
+    }
+}
+
+pub fn ip_address() {
+    let ifaces = datalink::interfaces();
+    for iface in ifaces {
+        for ip in iface.ips {
+            let ip_version = match ip.ip() {
+                IpAddr::V4(_) => "IPv4",
+                IpAddr::V6(_) => "IPv6",
+            };
+            let ip_string = ip.ip().to_string();
+            println!("{} {}", ip_version, ip_string);
+        }
+    }
+}
