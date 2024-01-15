216f11c720ee1d4fa7adf4fe57ff6f9640ad65f1
xTekC <xtekc.dev@pm.me>
2024-01-15 02:22:19 -0500

    refactor(core)

--- src/xcore/core.rs
+++ src/xcore/core.rs
@@ -5,43 +5,71 @@
  * https://www.mozilla.org/en-US/MPL/2.0/ *
  ******************************************/
 
+use owo_colors::{self, OwoColorize};
 use pnet::datalink;
 use std::net::IpAddr;
 
-pub fn interface_name() {
+pub fn active_interface_name() {
     for iface in datalink::interfaces() {
-        let name = iface.name;
-        println!("{}", name);
+        // Check if the interface has an IP starting with 192.168
+        if iface.ips.iter().any(|ip| matches!(ip.ip(), IpAddr::V4(ipv4_addr) if ipv4_addr.to_string().starts_with("192.168"))) {
+            println!("{}", iface.name.yellow());
+        }
     }
 }
 
-pub fn status_of_interface() {
-    for iface in datalink::interfaces() {
-        let status = iface.is_up();
-        println!("Up: {}", status);
+pub fn active_ipv4_address() {
+    let ifaces = datalink::interfaces();
+    for iface in ifaces {
+        for ip in iface.ips {
+            if let IpAddr::V4(ipv4_addr) = ip.ip() {
+                if ipv4_addr.to_string().starts_with("192.168") {
+                    println!("IPv4: {}", ipv4_addr.magenta());
+                }
+            }
+        }
     }
 }
 
-pub fn mac_address() {
+pub fn active_ipv6_address() {
     for iface in datalink::interfaces() {
-        let maca = match iface.mac {
+        // Check if the interface has an IPv4 address starting with 192.168
+        let has_ipv4_192 = iface.ips.iter().any(|ip| matches!(ip.ip(), IpAddr::V4(ipv4_addr) if ipv4_addr.to_string().starts_with("192.168")));
+
+        // If such an interface is found, print its IPv6 addresses
+        if has_ipv4_192 {
+            for ip in iface.ips {
+                if let IpAddr::V6(ipv6_addr) = ip.ip() {
+                    println!("IPv6: {}", ipv6_addr.to_string().magenta());
+                }
+            }
+        }
+    }
+}
+
+pub fn all_network_info() {
+    for iface in datalink::interfaces() {
+        let mac_address = match iface.mac {
             Some(mac) => mac.to_string(),
             None => String::from("None"),
         };
-        println!("MAC: {}", maca);
-    }
-}
 
-pub fn ip_address() {
-    let ifaces = datalink::interfaces();
-    for iface in ifaces {
+        println!(
+            "{}\nUp: {:?}\nMAC: {}",
+            iface.name.yellow(),
+            iface.is_up().green(),
+            mac_address.red()
+        );
+
         for ip in iface.ips {
             let ip_version = match ip.ip() {
                 IpAddr::V4(_) => "IPv4",
                 IpAddr::V6(_) => "IPv6",
             };
+
             let ip_string = ip.ip().to_string();
-            println!("{} {}", ip_version, ip_string);
+            println!("{}: {}", ip_version, ip_string.magenta());
         }
+        println!();
     }
 }
